5ba24e6615eac731b98a553c9435c9ea65ba86bc
Eric Oestrich <eric@oestrich.org>
2018-10-17 16:59:55 -0400

    Update release tasks for backbone repo

--- lib/grapevine/release_tasks.ex
+++ lib/grapevine/release_tasks.ex
@@ -9,14 +9,10 @@ defmodule Grapevine.ReleaseTasks do
     :ecto
   ]
 
-  @apps [
-    :grapevine
-  ]
-
-  @repos [
-    Backbone.Repo,
-    Grapevine.Repo
-  ]
+  @apps %{
+    backbone: [Backbone.Repo],
+    grapevine: [Grapevine.Repo]
+  }
 
   def migrate() do
     startup()
@@ -39,16 +35,20 @@ defmodule Grapevine.ReleaseTasks do
     # Start apps necessary for executing migrations
     Enum.each(@start_apps, &Application.ensure_all_started/1)
 
+    repos = List.flatten(Map.values(@apps))
+
     # Start the Repo(s) for grapevine
     IO.puts("Starting repos..")
-    Enum.each(@repos, & &1.start_link(pool_size: 1))
+    Enum.each(repos, & &1.start_link(pool_size: 1))
   end
 
   def priv_dir(app), do: "#{:code.priv_dir(app)}"
 
-  defp run_migrations_for(app) do
+  defp run_migrations_for({app, repos}) do
     IO.puts("Running migrations for #{app}")
-    Ecto.Migrator.run(Grapevine.Repo, migrations_path(app), :up, all: true)
+    Enum.each(repos, fn repo ->
+      Ecto.Migrator.run(repo, migrations_path(app), :up, all: true)
+    end)
   end
 
   defp migrations_path(app), do: Path.join([priv_dir(app), "repo", "migrations"])
